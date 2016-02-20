b058c779bc4a90f9750a420f228b52ca3b64ec3e
Joseph Trammel <jatrammel@gmail.com>
2016-02-20 03:21:10 -0600

    Trying to fix things

--- Public/mapFunctionality.js
+++ Public/mapFunctionality.js
@@ -1,59 +1,59 @@
 require([
-	"esri/map",
-	"esri/layers/FeatureLayer",
-	"esri/tasks/query",
-	"esri/tasks/QueryTask",
-	"esri/graphic",
-	"esri/symbols/SimpleFillSymbol",
-	"esri/symbols/SimpleLineSymbol",
-	"esri/geometry/Extent",
-	"esri/renderers/SimpleRenderer",
-	"esri/dijit/LocateButton",
+    "esri/map",
+    "esri/layers/FeatureLayer",
+    "esri/tasks/query",
+    "esri/tasks/QueryTask",
+    "esri/graphic",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/geometry/Extent",
+    "esri/renderers/SimpleRenderer",
+    "esri/dijit/LocateButton",
 
-	"dojo/dom",
-	"dojo/dom-construct",
-	"dojo/_base/Color",
-	"dijit/form/TextBox",
-	"dijit/form/Button",
-	"dojo/parser",
-	"dojo/_base/lang",
-	"dojo/domReady!"
+    "dojo/dom",
+    "dojo/dom-construct",
+    "dojo/_base/Color",
+    "dijit/form/TextBox",
+    "dijit/form/Button",
+    "dojo/parser",
+    "dojo/_base/lang",
+    "dojo/domReady!"
 ], function(
-	Map,
-	FeatureLayer,
-	Query,
-	QueryTask,
-	Graphic,
-	SimpleFillSymbol,
-	SimpleLineSymbol,
-	Extent,
-	SimpleRenderer,
-	LocateButton,
+    Map,
+    FeatureLayer,
+    Query,
+    QueryTask,
+    Graphic,
+    SimpleFillSymbol,
+    SimpleLineSymbol,
+    Extent,
+    SimpleRenderer,
+    LocateButton,
 
-	dom,
-	domConstruct,
-	Color,
-	TextBox,
-	dijitButton,
-	Parser,
-	lang
+    dom,
+    domConstruct,
+    Color,
+    TextBox,
+    dijitButton,
+    Parser,
+    lang
 ) {
-	// parser.parse();
+    // parser.parse();
 
-	map = new Map("map", {
-		basemap: "dark-gray",
-		center: [-95, 39], 					// longitude, latitude
-        zoom: 5,
-        fadeOnZoom: true,
-	});
+    map = new Map("map", {
+        basemap: "dark-gray",
+        center: [-95, 39],                     // longitude, latitude
+       zoom: 5,
+       fadeOnZoom: true,
+    });
 
-	var locate = new LocateButton({
-	map: map,
-	highlightLocation: true
+    var locate = new LocateButton({
+    map: map,
+    highlightLocation: true
 }, 'LocateButton');
 
 map.on("click", function(evt){
-	console.log(evt);
+    console.log(evt);
 });
 
 });
