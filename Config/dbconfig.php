d7f8448a5400f7804e222d7c7230f277cb9dc089
Joseph Trammel <jatrammel@gmail.com>
2016-02-20 00:51:19 -0600

    Added the dbconnect file

---
+++ Config/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+	$host = 'ec2-54-83-198-111.compute-1.amazonaws.com';
+	$port = 5432;
+	$dbname = 'd73o7nba6b5dnd';
+	$username = 'ssoizhnbezstms';
+	$password = 'k9bmVMVWG8Q57mXs5npXz_dpYq';
+?>
